1a9bb4d7b21b31e5d7ec433c50d6a8f086d05b42
Caerglad <caerglad@gmail.com>
2019-12-04 09:41:01 +0100

    Day 4

--- d4/src/PasswordSearch.java
+++ d4/src/PasswordSearch.java
@@ -1,9 +1,38 @@
+
 public class PasswordSearch {
     private static final int MIN_VALUE = 372037;
     private static final int MAX_VALUE = 905157;
 
 
     public int getCombinations() {
-        return 0;
+        int combinations = 0;
+        for (int i = MIN_VALUE; i <= MAX_VALUE; i++) {
+            String password = Integer.toString(i);
+            if (checkDuplicates(password) && checkOrder(password)) {
+                combinations++;
+                System.out.println(password);
+            }
+
+        }
+        return combinations;
     }
+
+    private boolean checkOrder(String password) {
+        for (int i = 1; i < 6; i++) {
+            if (password.charAt(i-1) > password.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    private boolean checkDuplicates(String password) {
+        int count = 1;
+        for (int i = 1; i < 6; i++) {
+            if (password.charAt(i-1) == password.charAt(i)) {
+                count++;
+            } else if (count == 2) return true;
+            else count = 1;
+        }
+        return count == 2;
+    }
+
 }
